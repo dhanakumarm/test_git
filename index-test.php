0ba2a5f2404ce433c06ce7b25dae23dbb33cecb6
Dhana <tmdhanakumar@gmail.com>
2019-12-23 22:29:10 +0530

    test

--- index-test.php
+++ index-test.php
@@ -7,7 +7,7 @@
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../../framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/test.php';
-
+//test
 // remove the following line when in production mode
 // defined('YII_DEBUG') or define('YII_DEBUG',true);
 
